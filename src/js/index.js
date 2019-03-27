21615d8e203dc97e3352b9a54515f4960bde4cd4
namezlx <1879558202@qq.com>
2019-03-27 10:52:46 +0800

    v0.01

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+(function(window){
+    var result='absckd';
+    var obj={
+        name:'lisi',
+        age:20
+    }
+    console.log(result);
+    console.log(obj.name);
+})(window);
---
+++ src/js/login.js
@@ -0,0 +1,7 @@
+(function($){
+    var $box=$('.box');
+    var $btn=$('ul li');
+    $btn.on('click',function(){
+        $box.css=('width','100px');
+    });
+})(jquery);
